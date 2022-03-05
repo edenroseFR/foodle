714de3ac4f932c247a30cfe9ae83a4c689feb8cd
edenroseFR <edenroserisma13@gmail.com>
2022-03-05 17:08:07 +0800

    Setup blueprint

---
+++ config.py
@@ -0,0 +1,8 @@
+from os import getenv
+
+SECRET_KEY = getenv("SECRET_KEY")
+DB_NAME = getenv("DB_NAME")
+DB_USERNAME = getenv("DB_USERNAME")
+DB_PASSWORD = getenv("DB_PASSWORD")
+DB_HOST = getenv("DB_HOST")
+DB_PORT = getenv("DB_PORT")
