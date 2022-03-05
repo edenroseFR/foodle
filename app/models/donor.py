b98327d0ad3e23764a6e4fa76e501f2eb097c5c5
gerald saberon <gerald.saberon@g.msuiit.edu.ph>
2022-03-05 18:56:31 +0800

    made changes/additions to donor model

--- app/models/donor.py
+++ app/models/donor.py
@@ -101,3 +101,211 @@ class Donor:
         cursor.execute(query_2, [user_id])
         results = cursor.fetchall()
         return results
+
+    def get_my_donations(self, user_id, role="donor"):
+        donations = {}
+        if role == "donor":
+            query = f"""
+                SELECT 
+                    d.donation_id, 
+                    d.transport_mode,
+                    u.first_name, 
+                    u.middle_name, 
+                    u.last_name, 
+                    u.org_name,
+                    d.datetime_created, 
+                    i.category,
+                    i.item_img_url,
+                    i.quantity,
+                    i.unit
+                FROM donations d
+                JOIN users u ON d.donor_id = u.user_id
+                JOIN items i ON i.donation_id = d.donation_id
+                WHERE d.donor_id = {user_id} and is_posted = 1
+            """
+        else:
+            query = f"""
+                SELECT 
+                    d.donation_id, 
+                    d.transport_mode,
+                    u.first_name, 
+                    u.middle_name, 
+                    u.last_name, 
+                    u.org_name,
+                    d.datetime_created, 
+                    i.category,
+                    i.item_img_url,
+                    i.quantity,
+                    i.unit
+                FROM collected_donations cd
+                JOIN donations d ON cd.donation_id = d.donation_id
+                JOIN users u ON cd.collector_id = u.user_id
+                JOIN items i ON i.donation_id = d.donation_id
+                WHERE cd.collector_id = {user_id}
+            """
+        cursor.execute(query)
+        result = cursor.fetchall()
+        result = [list(i) for i in result]
+
+        for item in result:
+            donation_id = item[0]
+            transport_name = self.get_transport_mode_name(item[1])
+            category = self.get_item_category(item[7])
+            image = item[8]
+            time_posted = item[6].strftime("%m/%d/%Y, %H:%M:%S")
+            distribution_photos = []
+            available = False
+
+            # Concatenate quantity and unit
+            if int(item[10]) == 1:
+                quantity = "kilo/s"
+            else:
+                quantity = "liter/s"
+
+            if donation_id not in donations:
+                # Check for organization_name
+                if item[5]:
+                    donor_name = item[5]
+                else:
+                    donor_name = item[2] + " " + item[3] + " " + item[4]
+
+                # Check donation status
+                status = self.get_status(donation_id)
+                if status == "distributed":  # get distribution photos
+                    distribution_photos = self.get_distribution_photos(donation_id)
+                elif status == "available":  # set available to True
+                    available = True
+
+                donations[donation_id] = {
+                    "donor_name": donor_name,
+                    "transport_mode": transport_name,
+                    "time_posted": time_posted,
+                    "items": [[category, item[9], quantity, image]],
+                    "distribution_photos": distribution_photos,
+                    "available": available,
+                }
+            else:
+                donations[donation_id]["items"].append(
+                    [category, item[9], quantity, image]
+                )
+        return donations
+
+    def get_transport_mode_name(self, mode_id):
+        query = f"""
+            SELECT mode_name
+            FROM transport_modes
+            WHERE mode_id = '{mode_id}'
+        """
+        cursor.execute(query)
+        mode_name = cursor.fetchone()[0]
+        return mode_name
+
+    def get_status(self, donation_id):
+        query = f"""
+            SELECT is_posted, 
+                   is_collected, 
+                   is_distributed
+            FROM donations 
+            WHERE donation_id = {donation_id}
+        """
+        cursor.execute(query)
+        status = list(cursor.fetchall()[0])
+        if status[0]:  # Still available for grab
+            return "available"
+        elif status[2]:  # Already distributed
+            return "distributed"
+        else:  # Not distributed but already collected
+            return "collected"
+
+    def get_distribution_photos(self, donation_id):
+        query = f"""
+            SELECT image_link
+            FROM distribution_images
+            WHERE donation_id = {donation_id}
+        """
+        cursor.execute(query)
+        images = cursor.fetchall()
+        images = [str(image[0]) for image in images]
+        return images
+
+    def get_item_category(self, category_id):
+        query = f"""
+            SELECT category_name
+            FROM categories
+            WHERE category_id = '{category_id}'
+        """
+        cursor.execute(query)
+        mode_name = cursor.fetchone()[0]
+        return mode_name
+
+    @staticmethod
+    def get_donor_id(session_email):
+        query = f"""
+            SELECT user_id
+            FROM users
+            WHERE email = '{session_email}'
+        """
+        cursor.execute(query)
+        donor_id = cursor.fetchone()[0]
+        return donor_id
+
+    @staticmethod
+    def delete_donation(donation_id):
+        query1 = f"""
+            DELETE FROM donations
+            WHERE donation_id = {donation_id}
+        """
+        query2 = f"""
+            DELETE FROM items
+            WHERE donation_id = {donation_id}
+        """
+        cursor.execute(query2)
+        db.commit()
+        cursor.execute(query1)
+        db.commit()
+        return None
+
+    @staticmethod
+    def get_collectors(session_email) -> list:
+        query_1 = "select users.user_id from users where email = %s"
+        cursor.execute(query_1, [session_email])
+        temp = cursor.fetchone()
+        user_id = temp[0]
+
+        collectors = {}
+        query = f"""
+            SELECT u.user_id,
+                   u.image_link,
+                   u.first_name, 
+                   u.middle_name, 
+                   u.last_name, 
+                   u.org_name,
+                   u.about
+            FROM collected_donations cd
+            JOIN users u on cd.collector_id = u.user_id
+            JOIN donations d ON cd.donation_id = d.donation_id
+            WHERE d.donor_id = {user_id} AND d.is_collected = 1
+        """
+        cursor.execute(query)
+        result = cursor.fetchall()
+        result = [list(collector) for collector in result]
+        for collector in result:
+            collector_image = collector[1]
+            org_name = collector[5]
+            collector_id = collector[0]
+            if org_name and org_name not in collectors:
+                collector_name = org_name
+                collectors[collector_name] = [
+                    collector_image,
+                    collector_id,
+                    collector[6],
+                ]
+            elif not org_name:
+                collector_name = collector[2] + " " + collector[3] + " " + collector[4]
+                if collector_name not in collectors:
+                    collectors[collector_name] = [
+                        collector_image,
+                        collector_id,
+                        collector[6],
+                    ]
+        return collectors
