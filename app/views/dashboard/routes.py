7f8c4566ce28eacd99fb652a85fbaad038ef3a36
gerald saberon <gerald.saberon@g.msuiit.edu.ph>
2022-03-07 02:59:19 +0800

    added 'to distribute' and 'completed' page for collector

--- app/views/dashboard/routes.py
+++ app/views/dashboard/routes.py
@@ -4,6 +4,23 @@ from . import dashboard_bp
 from flask import render_template, session, url_for, request, flash
 from . import utils
 
+@dashboard_bp.route('/donor/dashboard')
+def donor_dashboard() -> str:
+    if 'email_address' in session:
+        donor_details =  utils.show_donor_details(session["email_address"])
+        donations_kls = utils.get_total_foods(session["email_address"])
+        beverages_ltr = utils.get_total_beverages(session["email_address"])
+        food_distributed = utils.get_total_food_distributed(session["email_address"])
+        bev_distributed = utils.get_total_bev_distributed(session["email_address"])
+        collectors = utils.get_collectors(session["email_address"])
+       
+
+        return render_template('dashboard/donor_dashboard.html', donor = donor_details, 
+            donations_kls = donations_kls,  beverages_ltr = beverages_ltr,  bev_distributed = bev_distributed,
+            food_distributed=food_distributed, collectors = collectors) 
+    else:
+        return render_template('login/base.html')
+
 @dashboard_bp.route('/collector/dashboard')
 def collector_dashboard():
     if 'email_address' in session:
@@ -11,17 +28,11 @@ def collector_dashboard():
         food_distributed = utils.get_total_food_bev_distributed(session["email_address"])
         our_donors = utils.get_organization_donors(session["email_address"])
        
-        return redirect(url_for("collectors_view.collectors_view"))
+        return render_template('dashboard/collector_dashboard.html', donor = donor_details, 
+            our_donors=our_donors, food_distributed=food_distributed) 
     else:
         return redirect(url_for('login.login'))
 
-@dashboard_bp.route('/collector/dashboard/to recieve')
-def to_recieve():
-    if 'email_address' in session:
-        donor_details =  utils.show_donor_details(session["email_address"])
-        reserves = utils.get_items_to_collect(session["email_address"])
-        reserves.reverse()
-        return render_template('dashboard/org_to_collect.html', donor = donor_details, reserves=reserves)
 
 @dashboard_bp.route('/collector/dashboard/to distribute')
 def to_distribute():
@@ -31,13 +42,33 @@ def to_distribute():
         collections.reverse()
         return render_template('dashboard/org_to_distribute.html', donor = donor_details, collections=collections)
 
-@dashboard_bp.route('/collector/dashboard/completed collection')
-def completed_collection():
+@dashboard_bp.route('/collector/dashboard/distributed', methods=['POST'])
+def distribute():
+    if 'email_address' in session:
+        image_link1 = request.files['image_file1']
+        image_link2 = request.files['image_file2']
+        image_link3 = request.files['image_file3']
+        img_storage = [image_link1, image_link2, image_link3]
+        images = []
+        for image in img_storage:
+            if image.filename:
+                img_link = utils.get_image_url(image)
+                images.append(img_link)
+        donation_id = request.args.get('id')
+        utils.set_distributed_to_true(donation_id)
+        utils.save_distribution_images(donation_id, images)
+        utils.record_distribution(donation_id)
+        return redirect(url_for('dashboard.to_distribute'))
+
+
+@dashboard_bp.route('/collector/dashboard/to recieve')
+def to_recieve():
     if 'email_address' in session:
         donor_details =  utils.show_donor_details(session["email_address"])
-        completed = utils.get_completed_collections(session['email_address'])
-        completed.reverse()
-        return render_template('dashboard/org_completed.html', donor = donor_details, completed=completed)
+        reserves = utils.get_items_to_collect(session["email_address"])
+        reserves.reverse()
+        return render_template('dashboard/org_to_collect.html', donor = donor_details, reserves=reserves)
+
 
 @dashboard_bp.route('/collector/dashboard/recieved or collected')
 def recieve_collect():
@@ -46,6 +77,15 @@ def recieve_collect():
         utils.set_collected_to_true(donation_id)
         return redirect(url_for('dashboard.to_recieve'))
 
+@dashboard_bp.route('/collector/dashboard/completed collection')
+def completed_collection():
+    if 'email_address' in session:
+        donor_details =  utils.show_donor_details(session["email_address"])
+        completed = utils.get_completed_collections(session['email_address'])
+        completed.reverse()
+        return render_template('dashboard/org_completed.html', donor = donor_details, completed=completed)
+
+        
 @dashboard_bp.route('/collector/dashboard/cancel')
 def collect_cancel():
     if 'email_address' in session:
--- app/views/dashboard/utils.py
+++ app/views/dashboard/utils.py
@@ -9,16 +9,108 @@ def show_donor_details(session_email):
     donor = Donor()
     return donor.get_donor_details(session_email)
 
-def get_total_food_bev_distributed(email_address):
-    total_distribution = Collector().get_total_food_bev_distributed(email_address)
-    return total_distribution
+def get_total_foods(session_email):
+    total_kilos = 0
+    donor= Donor()
+    kilos = donor.donation_stat_foods(session_email)
+    for item in kilos:
+        total_kilos += item[0]
 
+    return total_kilos
+
+def get_total_beverages(session_email):
+    total_liters = 0
+    donor= Donor()
+    liters = donor.donation_stat_beverages(session_email)
+    for item in liters:
+        total_liters += item[0]
+
+    return total_liters
+
+def get_total_food_distributed(session_email):
+    total_kilos = 0
+    donor= Donor()
+    kilos = donor.donation_stat_food_distributed(session_email)
+    for item in kilos:
+        total_kilos += item[0]
+
+    return total_kilos
+
+def get_total_bev_distributed(session_email):
+    total_liters = 0
+    donor= Donor()
+    liters = donor.donation_stat_bev_distributed(session_email)
+    for item in liters:
+        total_liters += item[0]
+
+    return total_liters
+
+
+# for collector's dashboard
 def get_organization_donors(session_email):
     collector_id = Collector().get_id(session_email)
     donors = Collector().get_donors(collector_id)
     return donors
 
+   
+def get_items_for_distribution(session_email):
+    collector_id = Collector().get_id(session_email)
+    items_for_distribution = Collector().get_to_distribute(collector_id)
+    return items_for_distribution
+
+def set_distributed_to_true(donation_id):
+    Collector().set_distribute_to_true(donation_id)
+    return None
+
+
 def get_items_to_collect(session_email):
     collector_id = Collector().get_id(session_email)
     items_to_collect = Collector().get_items_to_collect(collector_id)
     return items_to_collect
+
+
+def set_collected_to_true(donation_id):
+    collector_id = Collector().get_id(session['email_address'])
+    date_collected = datetime.datetime.now()
+    Collector().set_collected_to_true(donation_id, collector_id, date_collected)
+    return None
+
+def get_collectors(email_address):
+    collectors = Donor().get_collectors(email_address)
+    return collectors
+
+#
+def get_collector_id():
+    collector_id = Collector().get_id(session['email_address'])
+
+    return collector_id
+
+    return None
+def get_completed_collections(email_address):
+    collector_id = Collector().get_id(email_address)
+    completed = Collector().get_completed_collections(collector_id)
+    return completed
+
+def get_image_url(file: FileStorage):
+    upload_image = cloud.upload(file)
+    url = upload_image.get('url')
+    return url
+
+def save_distribution_images(donation_id: int, images):
+    Collector().save_distribution_images(donation_id, images)
+    return None
+
+def record_distribution(donation_id: int) -> None:
+    collector_id = Collector().get_id(session['email_address'])
+    time_created = datetime.datetime.now()
+    Collector().record_distribution(donation_id, collector_id, time_created)
+
+def set_collected_to_false(donation_id):
+    collector_id = Collector().get_id(session['email_address'])
+    date_collected = datetime.datetime.now()
+    Collector().set_collected_to_false(donation_id)
+    return None
+
+def get_total_food_bev_distributed(email_address):
+    total_distribution = Collector().get_total_food_bev_distributed(email_address)
+    return total_distribution
